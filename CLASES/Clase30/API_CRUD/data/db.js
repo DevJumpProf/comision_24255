36b44b1e23828700d78eee6aa0689946af6eaf41
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2024-06-13 00:06:20 -0300

    crud

---
+++ CLASES/Clase30/API_CRUD/data/db.js
@@ -0,0 +1,11 @@
+ const {Sequelize} = require ("sequelize")
+
+/*  nombre de la base de datos -  user - contraseña - {donde esta alojada?,lenguaje, puerto} */
+
+const db = new Sequelize ("posteos24255","root","",{
+    host: "localhost",
+    dialect: "mysql",
+    port:3306
+})
+
+module.exports= db
